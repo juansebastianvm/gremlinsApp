fc8951c7fc03f10e3bd15e1a601876afdea3921f
Juan Sebastian <juansebastianvm@hotmail.es>
2023-03-06 17:02:00 -0500

    Control de errores BD

--- src/model/Conexion.php
+++ src/model/Conexion.php
@@ -3,18 +3,25 @@ class Conexion {
 
     private $mysqli;
     private $resultado;
+    private $estado;
+
+    function getEstado() {
+        return $this -> estado;
+    }
 
     function abrir () {
         $host="localhost";
         $user="gremlinsUserDB";
         $pass="Y)ig(hyo5IXe4PV3";
         $db="gremlinsdb";
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $this -> mysqli = new mysqli ($host,$user,$pass,$db);
         $this -> mysqli -> set_charset("utf8");
     }
 
     function ejecutar($sql) {
-        $this -> resultado = $this -> mysqli -> query($sql);        
+        $this -> resultado = $this -> mysqli -> query($sql);   
+        $this -> estado =  mysqli_connect_errno() . " " . mysqli_connect_error();
     }
 
     function cerrar() {
@@ -28,6 +35,7 @@ class Conexion {
 
     function extraer() {
         return $this -> resultado -> fetch_row();
+        $this -> estado =  mysqli_connect_errno() . " " . mysqli_connect_error();
     }
  
     function ultimoId() {
