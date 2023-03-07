b2800573f6980d0153277ba4b46d90d18d6d2243
Juan Sebastian <juansebastianvm@hotmail.es>
2023-03-07 11:04:13 -0500

    IP 2

--- src/controller/Router.php
+++ src/controller/Router.php
@@ -1,6 +1,6 @@
 <?php
 
 //$Router = "34.168.147.27";
-$Router = "10.138.0.2"; // IP LOCAL MV UBUNTU SERVER 20.04 Google Cloud
+$Router = "34.83.183.36"; // IP LOCAL MV UBUNTU SERVER 20.04 Google Cloud
 
 ?>
