b2f934f43598c6c92c387d03aa3acabf5f122908
Juan Sebastian <juansebastianvm@hotmail.es>
2023-03-07 12:20:50 -0500

    Prueba error Controladores

--- src/controller/RolCTL.php
+++ src/controller/RolCTL.php
@@ -3,6 +3,8 @@ session_start();
 require_once("../model/Rol.php");
 require_once("Router.php");
 
+echo "HOLA";
+
 if (isset($_POST["guardar"])) {
 
     if (isset($_POST["gr_role_id"]) && ($_POST["gr_role_id"] != "")) {
